6cfe5dc27e9745d3d4ca96d80249329ec8de7c98
Ayaan <146638318+AyaanIlahi@users.noreply.github.com>
2025-03-05 01:46:09 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import { redis } from './databaseCalls/connections.js'; // Import Redis connection
+import rateLimiter from './middlewareRedis.js'; //Import Rate Limiter
+import axios from 'axios';
+import pokemonRoutes from "./routes/pokemonRouteHandle.js";
+import unsplashRoute from './routes/unsplashRouteHandle.js';  // Import Unsplash API routes
+import cookieParser from 'cookie-parser';
+import createTokenRoute  from './routes/createTokenRouteHandle.js'; 
+import dotenv from 'dotenv';
+dotenv.config({ path: './process.env' });
+import cors from'cors';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",");
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());  // To handle cookies for JWT
+
+app.use("/pokemon", pokemonRoutes);
+
+// Mount Unsplash API routes
+app.use('/imagesearch', unsplashRoute);
+
+// Route to create a user and generate a JWT
+app.use('/createToken', createTokenRoute);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
